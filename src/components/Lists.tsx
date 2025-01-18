7f24504ba107da58c5637773dc1605ea30e3d5e7
Sarah <srh.colin@gmail.com>
2025-01-18 16:20:02 +0100

    Noms des listes apparaissent

--- src/components/Lists.tsx
+++ src/components/Lists.tsx
@@ -1,13 +1,22 @@
 import { listsData } from "../data/listsData"
 
 export default function Lists() {
+    function ClickMe() {
+        return console.log("Cliqué")
+    }
   return (
-    <ul className="flex flex-col">
-        {listsData.map((list) => (
-            <li key={list.id}>
-                {list.listName}
-            </li>
-        ))}
-    </ul>
+    <div>
+        <div className="flex justify-between">
+            <h1>Vos listes de courses</h1>
+            <button onClick={ClickMe}>Créer une nouvelle liste </button>
+        </div>
+        <ul className="flex flex-col gap-2">
+            {listsData.map((list) => (
+                <li key={list.id} className="p-4 border-2 border-purple-500">
+                    {list.listName}
+                </li>
+            ))}
+        </ul>
+    </div>
   )
 }
